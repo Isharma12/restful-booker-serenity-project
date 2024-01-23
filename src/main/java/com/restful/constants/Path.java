0333605f1f42a09ae0f4aed6e811a903071686c3
Isharma12 <indush8412@gmail.com>
2024-01-23 16:27:23 +0000

    Initial commit

---
+++ src/main/java/com/restful/constants/Path.java
@@ -0,0 +1,11 @@
+package com.restful.constants;
+
+public class Path {
+
+    /*
+     * This is base path for Booking api
+     */
+    public static final String BOOKING = "/booking";
+    public static final String AUTHORIZATION = "/auth";
+
+}
